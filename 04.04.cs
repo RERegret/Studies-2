f9f32948131fdf793386baa4b08fa2c6f6f8a1aa
RERegret <47378970+RERegret@users.noreply.github.com>
2019-04-04 11:02:54 +0200

    Add files via upload

---
+++ 04.04.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[] szamok = { 10, 15, 17, 20, 30, 40, 50, 60, 70 };
+        int[] lk = new int[szamok.Length];
+
+        int ujindex = 0;
+        for (int i = 0; i < szamok.Length; i++)
+        {
+            if (szamok[i]%2==0)
+            {
+                lk[ujindex] = szamok[i];
+                ujindex++;
+            }
+        }
+
+
+        Console.ReadKey();
+    }
+}
+
